c2101ff276c48d549ee8c93cb42e859d44295f29
haleyvferro <haleyvferro@gmail.com>
2020-07-27 22:55:47 -0500

    finished controllers maybe

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -9,22 +9,28 @@ class ExhibitsController < ApplicationController
     end
 
     def create
-
+        @exhibit = Exhibit.create(exhibit_params)
+        redirect_to exhibit_path(@exhibit)
     end
 
     def show
+        @exhibit = Exhibit.find(params[:id])
     end
 
     def edit
+        @exhibit = Exhibit.find(params[:id])
     end
 
     def update
+        @exhibit = Exhibit.find(params[:id])
+        @exhibit.update(exhibit_params)
+        redirect_to exhibit_path(@exhibit)
     end
 
     private
 
     def exhibit_params
-        params.require(:exhibit).permit()
+        params.require(:exhibit).permit(:name, :picture, :category, :date, :museum_id)
     end
 
 end
