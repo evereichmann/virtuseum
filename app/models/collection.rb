6e1d9b7fc4682c125b2c83c4b7eca14820433a84
Eve Reichmann <eve.reichmann@gmail.com>
2020-07-29 22:31:10 -0500

    layout menu work

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,10 @@ class Collection < ApplicationRecord
     has_many :items
 
     accepts_nested_attributes_for :items, allow_destroy: true
+
+    def self.find_name
+        Collection.all.map do |exhibit|
+            exhibit.name
+        end    
+    end
 end
