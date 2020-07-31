03ecb74f3d37c563f74ce2e0dacea4c54a1c6ada
Eve Reichmann <eve.reichmann@gmail.com>
2020-07-31 09:36:06 -0500

    removed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
- @contemporary_art = ["https://i.pinimg.com/236x/9c/84/94/9c8494d200d49913a256a69a0b683bde.jpg", "https://i.pinimg.com/236x/58/ec/b8/58ecb8829a54cfc446808144c27d52fb.jpg”, “https://i.pinimg.com/236x/c5/2a/13/c52a13a6d2cc93f991720f6866100f33.jpg", "https://i.pinimg.com/236x/82/44/19/8244192c6c25f9cfbb35d80807e4636c.jpg", "https://i.pinimg.com/236x/08/a4/3f/08a43fa081747b31bf2d76d57f5fd2f4.jpg", "https://i.pinimg.com/236x/57/d6/dd/57d6dd9cd3443a15e48b28e6b6e27aca.jpg", "https://i.pinimg.com/236x/73/5a/d4/735ad48a85ddde0016cdf43d0a5acf44.jpg", 
- "https://i.pinimg.com/236x/83/3e/c3/833ec3b849e4ff710cf904e1c1c6c22f.jpg", "https://i.pinimg.com/236x/fc/7d/e6/fc7de6df00a081cd2c0b8f63ec89337f.jpg", "https://i.pinimg.com/236x/36/c8/13/36c813e94ff57108be60ebf90ecaabd7.jpg", "https://i.pinimg.com/236x/51/37/c9/5137c9e6ae7b7fc17da8eb679519af5b.jpg", "https://i.pinimg.com/236x/6c/57/86/6c5786307e284f8e36cd88c31f056814.jpg", "https://i.pinimg.com/236x/f2/ba/cd/f2bacdfc71831ab6a07c01f3dbf0253a.jpg", "https://i.pinimg.com/236x/b9/9c/20/b99c2080209d7f1a6fab142402b9bb89.jpg", 
- "https://i.pinimg.com/236x/2b/a8/e8/2ba8e8be0c8b365635d9c0abe60251b9.jpg", "https://i.pinimg.com/236x/ad/88/07/ad88073f3914f405dda88602eb961763.jpg", "https://i.pinimg.com/236x/06/f2/19/06f21932ad221dbcc22d14ac3f200672.jpg", "https://i.pinimg.com/236x/95/8a/4b/958a4b7d96c2eeee81cc7e5c902aed74.jpg", "https://i.pinimg.com/236x/1a/5c/cf/1a5ccf0ce56edda04adf65f77cb8804e.jpg", "https://i.pinimg.com/236x/89/da/53/89da53589e0f425987ef80879fe08289.jpg", "https://i.pinimg.com/236x/25/c3/c2/25c3c28022782e1f4bd34218494cab31.jpg", "https://i.pinimg.com/236x/83/6b/6d/836b6d35c11e1530976b1b4c345c172a.jpg", "https://i.pinimg.com/236x/d7/ff/aa/d7ffaa6bbd89ebcb365fd6a7d67aaf02.jpg",
-  "https://i.pinimg.com/236x/53/73/5f/53735fb5161df512091752f18e2e5615.jpg", "https://i.pinimg.com/236x/83/7b/27/837b270a150831688c9a86113a64d291.jpg", "https://i.pinimg.com/236x/0a/2f/78/0a2f78982ad9555e8ff6a025dc0dfc68.jpg", "https://i.pinimg.com/236x/a0/82/ac/a082aca4d7c9e084977d95f335136ae5.jpg" ]
+#  @contemporary_art = ["https://i.pinimg.com/236x/9c/84/94/9c8494d200d49913a256a69a0b683bde.jpg", "https://i.pinimg.com/236x/58/ec/b8/58ecb8829a54cfc446808144c27d52fb.jpg”, “https://i.pinimg.com/236x/c5/2a/13/c52a13a6d2cc93f991720f6866100f33.jpg", "https://i.pinimg.com/236x/82/44/19/8244192c6c25f9cfbb35d80807e4636c.jpg", "https://i.pinimg.com/236x/08/a4/3f/08a43fa081747b31bf2d76d57f5fd2f4.jpg", "https://i.pinimg.com/236x/57/d6/dd/57d6dd9cd3443a15e48b28e6b6e27aca.jpg", "https://i.pinimg.com/236x/73/5a/d4/735ad48a85ddde0016cdf43d0a5acf44.jpg", 
+#  "https://i.pinimg.com/236x/83/3e/c3/833ec3b849e4ff710cf904e1c1c6c22f.jpg", "https://i.pinimg.com/236x/fc/7d/e6/fc7de6df00a081cd2c0b8f63ec89337f.jpg", "https://i.pinimg.com/236x/36/c8/13/36c813e94ff57108be60ebf90ecaabd7.jpg", "https://i.pinimg.com/236x/51/37/c9/5137c9e6ae7b7fc17da8eb679519af5b.jpg", "https://i.pinimg.com/236x/6c/57/86/6c5786307e284f8e36cd88c31f056814.jpg", "https://i.pinimg.com/236x/f2/ba/cd/f2bacdfc71831ab6a07c01f3dbf0253a.jpg", "https://i.pinimg.com/236x/b9/9c/20/b99c2080209d7f1a6fab142402b9bb89.jpg", 
+#  "https://i.pinimg.com/236x/2b/a8/e8/2ba8e8be0c8b365635d9c0abe60251b9.jpg", "https://i.pinimg.com/236x/ad/88/07/ad88073f3914f405dda88602eb961763.jpg", "https://i.pinimg.com/236x/06/f2/19/06f21932ad221dbcc22d14ac3f200672.jpg", "https://i.pinimg.com/236x/95/8a/4b/958a4b7d96c2eeee81cc7e5c902aed74.jpg", "https://i.pinimg.com/236x/1a/5c/cf/1a5ccf0ce56edda04adf65f77cb8804e.jpg", "https://i.pinimg.com/236x/89/da/53/89da53589e0f425987ef80879fe08289.jpg", "https://i.pinimg.com/236x/25/c3/c2/25c3c28022782e1f4bd34218494cab31.jpg", "https://i.pinimg.com/236x/83/6b/6d/836b6d35c11e1530976b1b4c345c172a.jpg", "https://i.pinimg.com/236x/d7/ff/aa/d7ffaa6bbd89ebcb365fd6a7d67aaf02.jpg",
+#   "https://i.pinimg.com/236x/53/73/5f/53735fb5161df512091752f18e2e5615.jpg", "https://i.pinimg.com/236x/83/7b/27/837b270a150831688c9a86113a64d291.jpg", "https://i.pinimg.com/236x/0a/2f/78/0a2f78982ad9555e8ff6a025dc0dfc68.jpg", "https://i.pinimg.com/236x/a0/82/ac/a082aca4d7c9e084977d95f335136ae5.jpg" ]
 
-@people = ["https://evada-images.global.ssl.fastly.net/76d1ea39-a4eb-4270-b9dc-899653415f8f/home-tile-person-3.jpg?width=345&height=345", "https://i.pinimg.com/236x/c0/43/80/c04380a7c8692b6cb0124e02cbc743f3.jpg", 
-"https://i.pinimg.com/236x/93/de/47/93de47eaee32e0a80475e124f7392683.jpg", "https://i.pinimg.com/236x/5f/c1/c8/5fc1c898e7ecdcbd9ff9d854dcd5b58b.jpg", "https://i.pinimg.com/236x/34/6b/59/346b597b5550b8384a298ff8a38eb818.jpg", 
-"https://i.pinimg.com/236x/94/f8/16/94f816241e2ce916725939133b6ef88f.jpg", "https://i.pinimg.com/236x/dc/21/65/dc2165b854bba0953b5c969e49fd4d88.jpg", 
-"https://i.pinimg.com/236x/2e/77/1e/2e771eca0c598d06a00c003689553f96.jpg", "https://www.pinterest.com/pin/612841461780793411/", "https://i.pinimg.com/236x/11/ed/08/11ed085f454d71e984d2609c2b147715.jpg", "https://i.pinimg.com/236x/51/b8/13/51b813b825bb6fa13536e0b2f3de782f.jpg", "https://i.pinimg.com/236x/c3/2a/0a/c32a0a605c15c6519e5c28341260d8bd.jpg", "https://i.pinimg.com/236x/f5/23/24/f523242eac5db76989e5b87c992598d6.jpg", 
-"https://i.pinimg.com/236x/3a/b1/ce/3ab1ce88c1d2bae5758ce1924c01cb20.jpg", "https://i.pinimg.com/236x/ca/4a/ab/ca4aab9c22d8092ccb3e503fce80e218.jpg", "https://i.pinimg.com/236x/61/10/b5/6110b57daf16f32b08f08504a96da89a.jpg", "https://i.pinimg.com/236x/6f/f6/2d/6ff62d6f4584ce9e628f1676847dad61.jpg", 
-"https://i.pinimg.com/236x/25/9f/76/259f76bfd1bbbff232893b5288734727.jpg", "https://i.pinimg.com/236x/52/ca/f2/52caf28b3bb4b69f60a9031d8eb88646.jpg", "https://i.pinimg.com/236x/48/0d/63/480d63a9910b26902232d953ea25c6d2.jpg", "https://i.pinimg.com/236x/14/a4/7e/14a47eb64de0041e516ba3037ec8baf9.jpg"]
+# @people = ["https://evada-images.global.ssl.fastly.net/76d1ea39-a4eb-4270-b9dc-899653415f8f/home-tile-person-3.jpg?width=345&height=345", "https://i.pinimg.com/236x/c0/43/80/c04380a7c8692b6cb0124e02cbc743f3.jpg", 
+# "https://i.pinimg.com/236x/93/de/47/93de47eaee32e0a80475e124f7392683.jpg", "https://i.pinimg.com/236x/5f/c1/c8/5fc1c898e7ecdcbd9ff9d854dcd5b58b.jpg", "https://i.pinimg.com/236x/34/6b/59/346b597b5550b8384a298ff8a38eb818.jpg", 
+# "https://i.pinimg.com/236x/94/f8/16/94f816241e2ce916725939133b6ef88f.jpg", "https://i.pinimg.com/236x/dc/21/65/dc2165b854bba0953b5c969e49fd4d88.jpg", 
+# "https://i.pinimg.com/236x/2e/77/1e/2e771eca0c598d06a00c003689553f96.jpg", "https://www.pinterest.com/pin/612841461780793411/", "https://i.pinimg.com/236x/11/ed/08/11ed085f454d71e984d2609c2b147715.jpg", "https://i.pinimg.com/236x/51/b8/13/51b813b825bb6fa13536e0b2f3de782f.jpg", "https://i.pinimg.com/236x/c3/2a/0a/c32a0a605c15c6519e5c28341260d8bd.jpg", "https://i.pinimg.com/236x/f5/23/24/f523242eac5db76989e5b87c992598d6.jpg", 
+# "https://i.pinimg.com/236x/3a/b1/ce/3ab1ce88c1d2bae5758ce1924c01cb20.jpg", "https://i.pinimg.com/236x/ca/4a/ab/ca4aab9c22d8092ccb3e503fce80e218.jpg", "https://i.pinimg.com/236x/61/10/b5/6110b57daf16f32b08f08504a96da89a.jpg", "https://i.pinimg.com/236x/6f/f6/2d/6ff62d6f4584ce9e628f1676847dad61.jpg", 
+# "https://i.pinimg.com/236x/25/9f/76/259f76bfd1bbbff232893b5288734727.jpg", "https://i.pinimg.com/236x/52/ca/f2/52caf28b3bb4b69f60a9031d8eb88646.jpg", "https://i.pinimg.com/236x/48/0d/63/480d63a9910b26902232d953ea25c6d2.jpg", "https://i.pinimg.com/236x/14/a4/7e/14a47eb64de0041e516ba3037ec8baf9.jpg"]
 
 
 User.destroy_all
@@ -18,33 +18,33 @@ Exhibit.destroy_all
 Museum.destroy_all
 Curator.destroy_all
 
-15.times do
-    User.create(username: Faker::Internet.username , password: Faker::Internet.password , name_first: Faker::Name.first_name , name_last: Faker::Name.last_name , location: Faker::Address.country, picture: @people.sample )
-end
+# 15.times do
+#     User.create(username: Faker::Internet.username , password: Faker::Internet.password , name_first: Faker::Name.first_name , name_last: Faker::Name.last_name , location: Faker::Address.country, picture: @people.sample )
+# end
 
-10.times do
-    Collection.create(name: Faker::Commerce.department ,user_id: User.all.sample.id )
-end
+# 10.times do
+#     Collection.create(name: Faker::Commerce.department ,user_id: User.all.sample.id )
+# end
 
-5.times do
-    Museum.create(name: Faker::Company.name, field_of_study: Faker::Commerce.department, information: Faker::String.random )       
-end
+# 5.times do
+#     Museum.create(name: Faker::Company.name, field_of_study: Faker::Commerce.department, information: Faker::String.random )       
+# end
 
-10.times do 
-    Exhibit.create(name: Faker::Company.industry , picture: @contemporary_art.sample , category: "contemporary art" , date: Faker::Time.forward , museum_id: Museum.all.sample.id)
-end
+# 10.times do 
+#     Exhibit.create(name: Faker::Company.industry , picture: @contemporary_art.sample , category: "contemporary art" , date: Faker::Time.forward , museum_id: Museum.all.sample.id)
+# end
 
-50.times do
-    Item.create(name: Faker::Commerce.product_name, year_created: -1960 , description: Faker::String.random , picture: @contemporary_art.sample , classification: "contemporary art", collection_id: Collection.all.sample.id)
-end
+# 50.times do
+#     Item.create(name: Faker::Commerce.product_name, year_created: -1960 , description: Faker::String.random , picture: @contemporary_art.sample , classification: "contemporary art", collection_id: Collection.all.sample.id)
+# end
 
-5.times do
-    Curator.create(user_id: User.all.sample.id, museum_id: Museum.all.sample.id)
-end
+# 5.times do
+#     Curator.create(user_id: User.all.sample.id, museum_id: Museum.all.sample.id)
+# end
 
-10.times do 
-    Loan.create(item_id: Item.all.sample.id, exhibit_id: Exhibit.all.sample.id)
-end    
+# 10.times do 
+#     Loan.create(item_id: Item.all.sample.id, exhibit_id: Exhibit.all.sample.id)
+# end    
 
 # User.create(name_first: "Emily", name_last: "Graslie", username: "brainscoop", password: "brainscoop",location: "Chicago, IL", picture: "https://lh3.googleusercontent.com/proxy/f_0myiT1lLpXHf27_OhEPFXsNn6cKN4Rv18ofHTRESb_6G7Xf9-nz8ZHtAS3PdBODgU4jSVuQSa6VszmT9lpRNCatavJrsG3KnD-eaHl-nbM7xY0HUg6eU6v1HtMnZWo7qY")
 # User.create(name_first: "Zahi", name_last: "Hawass", username: "mummyman", password: "kingtut", location: "Ciro, Egypt", picture: "https://upload.wikimedia.org/wikipedia/commons/a/ab/President_Barack_Obama_tours_the_Pyramids_and_Sphinx_with_Secretary_General_of_the_Egyptian_Supreme_Council_of_Antiquities_%28Hawass_cropped%29.jpg")
